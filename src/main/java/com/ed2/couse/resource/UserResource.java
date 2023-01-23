29fa93add840af072866e44ff49b186adf0bbb46
João Mateus Silva Lopes <61907813306@dpema.br>
2023-01-23 13:16:25 -0300

    add user

--- src/main/java/com/ed2/couse/resource/UserResource.java
+++ src/main/java/com/ed2/couse/resource/UserResource.java
@@ -4,18 +4,12 @@ import com.ed2.couse.entities.User;
 import com.ed2.couse.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
-
-
-
 public class UserResource {
     @Autowired
     private UserService service;
@@ -28,6 +22,12 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
         return ResponseEntity.ok().body(obj);
     }
 }
