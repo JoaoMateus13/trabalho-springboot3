de4555f566c1857078c9753464a30e4a2763ed2b
João Mateus <jms.lopes2001@gmail.com>
2023-01-24 19:27:25 -0300

    INSERT USER

--- src/main/java/com/ed2/couse/resource/UserResource.java
+++ src/main/java/com/ed2/couse/resource/UserResource.java
@@ -5,7 +5,9 @@ import com.ed2.couse.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +30,7 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
-        return ResponseEntity.ok().body(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
